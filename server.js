42eb2c9f6be934663304bd1068c0719e95ec4985
WaerWaters <daniel.johnsen1@hotmail.com>
2022-12-06 09:50:26 +0100

    v.1.2.4

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Datastore = require('nedb')
 require('dotenv').config()
+
 const app = express()
+const port = process.env.PORT || 3000
 
-
-app.listen(3000, function () {console.log('listening...')})
+app.listen(port, function () {console.log(`starting server at ${port}`)})
 app.use(express.static('public'))
 app.use(express.json())
 
